00d1d603d7cfe47a13f3ea553c38075c5393094c
rupesss-09 <rupesss09@gmail.com>
2021-03-24 01:45:09 -0500

    Rest Web Service - Get Method(QueryString/KeyValuePair)

--- webserviceprovide/src/main/java/com/example/demo/RestWebService.java
+++ webserviceprovide/src/main/java/com/example/demo/RestWebService.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,12 @@ public class RestWebService {
 	public int add(@PathVariable int abc, @PathVariable int pqr) throws Exception {
 		int sum=abc + pqr;
 		return sum;
+	}
+	
+	// http://localhost/subCal?param1=10&param2=5
+	@RequestMapping(value = "/subCal", method = RequestMethod.GET)
+	public int sub(@RequestParam("param1") int a, @RequestParam("param2") int b) {
+		int sub=a - b;
+		return sub;
 	}
 }
