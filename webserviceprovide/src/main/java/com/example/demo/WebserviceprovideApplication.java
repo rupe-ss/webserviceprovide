8f264419aa339b0c198b806e423c7c136949ea38
rupesss-09 <rupesss09@gmail.com>
2021-03-23 23:58:39 -0500

    WebService Launch

---
+++ webserviceprovide/src/main/java/com/example/demo/WebserviceprovideApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceprovideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceprovideApplication.class, args);
+	}
+
+}
